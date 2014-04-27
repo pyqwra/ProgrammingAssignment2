1d84d5725401e7a71d78c74bb3e66396116db73f
pyqwra <pyqwra@users.noreply.github.com>
2014-04-27 22:14:16 +0800

    Update MR-cachematrix.R

--- MR-cachematrix.R
+++ MR-cachematrix.R
@@ -1,7 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To write a function that can make an inverse matrix.
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = numeric()) {
   m <- NULL
